36eb91dda1add508110d637be8e7b817f4f6cefb
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 00:17:15 +0300

    Scripted auto-commit on change (2019-10-25 00:17:15) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -1,5 +1,8 @@
+import org.apache.spark.SparkConf;
+
 public class App {
     public static void main(String[] args){
-        
+        SparkConf conf = new SparkConf().setAppName("lab5");
+        JavaSparkContext sc = new JavaSparkContext(conf);
     }
 }
