2430b508a8ba951be78cbc19afe055877fdcb2e8
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 00:20:12 +0300

    Scripted auto-commit on change (2019-10-25 00:20:12) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -1,4 +1,5 @@
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
@@ -6,6 +7,7 @@ public class App {
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        
+        JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
+        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
     }
 }
