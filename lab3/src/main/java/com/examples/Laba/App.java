7e6d7b42ba3885492bf4f11e64b6493f6b1f610b
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 19:55:52 +0300

    Scripted auto-commit on change (2019-10-25 19:55:52) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/App.java
+++ lab3/src/main/java/com/examples/Laba/App.java
@@ -1,4 +1,38 @@
 package com.examples.Laba;
 
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+
 public class App {
+    private static String DELIMETR = "\"";
+    private static String EMPTY = "\"";
+    private static String COMMA = ",";
+    private static String FLIGHTINFO = "664600583_T_ONTIME_sample.csv";
+    private static String AIRPORTINFO = "L_AIRPORT_ID.csv";
+
+    private static String getSubstring(String s, int first, int second){
+        return s.substring(first,second);
+    }
+
+    public static void main(String[] args){
+        SparkConf conf = new SparkConf().setAppName("lab_3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> flightsFile = sc.textFile(FLIGHTINFO);
+        JavaRDD<String> airportFile = sc.textFile(AIRPORTINFO);
+
+        JavaRDD<String> AirData = airportFile
+                .mapToPair(s->{
+                    s = s.replaceAll(DELIMETR,EMPTY);
+                    int indexOfFirstComma = s.indexOf(COMMA);
+                    return new Tuple2<>(
+                            Integer.valueOf(getSubstring(s,0,indexOfFirstComma)),
+                            //getSubstring(s,indexOfFirstComma++,s.length()),
+                            s.substring(indexOfFirstComma+1,s.length())
+                    );
+                });
+    }
 }
