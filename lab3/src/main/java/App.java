443c3e9e37777f22f6ccc6994e24a4e28c9e7203
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 19:47:04 +0300

    Scripted auto-commit on change (2019-10-25 19:47:04) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -1,11 +1,17 @@
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import scala.Tuple1;
+import scala.Tuple2;
 
 public class App {
     private static String DELIMETR = "\"";
     private static String EMPTY = "\"";
+    private static String COMMA = ",";
+
+    private static String getSubstring(String s, int first, int second){
+        return s.substring(first,second);
+    }
+
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab_3");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -15,10 +21,12 @@ public class App {
 
         JavaRDD<String> AirData = airportFile
                 .mapToPair(s->{
-                   s= s.replaceAll(DELIMETR,EMPTY);
-                   s -> new Tuple1<>(
-                           Integer.valueOf()
-                   )
+                   s = s.replaceAll(DELIMETR,EMPTY);
+                    int indexOfFirstComma = s.indexOf(COMMA);
+                   s -> new Tuple2<>(
+                           Integer.valueOf(getSubstring(s,0,indexOfFirstComma)),
+                           getSubstring(s,indexOfFirstComma++,s.length());
+                   );
                 });
     }
 }
