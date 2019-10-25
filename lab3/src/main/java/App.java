7243db9aaf623bea69ca9c2ae37052d47753e810
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 19:48:54 +0300

    Scripted auto-commit on change (2019-10-25 19:48:54) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -3,10 +3,14 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class App {
     private static String DELIMETR = "\"";
     private static String EMPTY = "\"";
     private static String COMMA = ",";
+    private static String FLIGHTINFO = "664600583_T_ONTIME_sample.csv";
+    private static String AIRPORTINFO = "L_AIRPORT_ID.csv";
 
     private static String getSubstring(String s, int first, int second){
         return s.substring(first,second);
@@ -16,16 +20,16 @@ public class App {
         SparkConf conf = new SparkConf().setAppName("lab_3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
-        JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> flightsFile = sc.textFile(FLIGHTINFO);
+        JavaRDD<String> airportFile = sc.textFile(AIRPORTINFO);
 
         JavaRDD<String> AirData = airportFile
                 .mapToPair(s->{
                    s = s.replaceAll(DELIMETR,EMPTY);
-                    int indexOfFirstComma = s.indexOf(COMMA);
+                    AtomicInteger indexOfFirstComma = new AtomicInteger(s.indexOf(COMMA));
                    s -> new Tuple2<>(
-                           Integer.valueOf(getSubstring(s,0,indexOfFirstComma)),
-                           getSubstring(s,indexOfFirstComma++,s.length());
+                           Integer.valueOf(getSubstring(s,0, indexOfFirstComma.get())),
+                           getSubstring(s, indexOfFirstComma.getAndIncrement(),s.length());
                    );
                 });
     }
