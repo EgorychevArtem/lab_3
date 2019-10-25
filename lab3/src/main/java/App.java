67c01af01dc610caccb563c31aa1c75c7167691d
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 19:37:03 +0300

    Scripted auto-commit on change (2019-10-25 19:37:02) by gitwatch.sh

--- lab3/src/main/java/App.java
+++ lab3/src/main/java/App.java
@@ -3,13 +3,19 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class App {
+    private static String DELIMETR = "\"";
+    private static String EMPTY = "\"";
     public static void main(String[] args){
         SparkConf conf = new SparkConf().setAppName("lab_3");
         JavaSparkContext sc = new JavaSparkContext(conf);
 
         JavaRDD<String> flightsFile = sc.textFile("664600583_T_ONTIME_sample.csv");
-        JavaRDD<String> airportsFile = sc.textFile("L_AIRPORT_ID.csv");
+        JavaRDD<String> airportFile = sc.textFile("L_AIRPORT_ID.csv");
 
-        JavaRDD<String> 
+        JavaRDD<String> AirData = airportFile
+                .mapToPair(s->{
+                   s= s.replaceAll(DELIMETR,EMPTY);
+                   
+                });
     }
 }
