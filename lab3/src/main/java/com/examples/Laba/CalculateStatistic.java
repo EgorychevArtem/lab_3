4a3b96fe8a985427a32adfc21d76c5396b2b0723
Egorychev Artem <temych_2000@mail.ru>
2019-10-26 00:42:41 +0300

    Scripted auto-commit on change (2019-10-26 00:42:41) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/CalculateStatistic.java
+++ lab3/src/main/java/com/examples/Laba/CalculateStatistic.java
@@ -6,5 +6,10 @@ public class CalculateStatistic implements Serializable {
     float maxDelay;
     int Flightscount, DelayFlights, CancelFlights;
 
-    
+    public StatisticCount(int Flightscount, int CancelFlights, int DelayFlights, float maxDelay) {
+        this.maxDelay = maxDelay;
+        this.Flightscount = Flightscount;
+        this.CancelFlights = CancelFlights;
+        this.DelayFlights = DelayFlights;
+    }
 }
