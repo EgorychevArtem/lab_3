1c28403cc42193a48dcf2b72bd3b7b070f337f24
Egorychev Artem <temych_2000@mail.ru>
2019-10-26 01:29:00 +0300

    Scripted auto-commit on change (2019-10-26 01:29:00) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/App.java
+++ lab3/src/main/java/com/examples/Laba/App.java
@@ -87,30 +87,7 @@ public class App {
                     Tuple2<Integer, Integer> key = a._1();
                     String value = a._2();
 
-                    return "from: " + AiroportDestID.get(key._1()) + "to: " 
-                }
-        )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                    return "from: " + AiroportDestID.get(key._1()) + "to: " + AiroportDestID.get(key._2());
+                });
     }
 }
