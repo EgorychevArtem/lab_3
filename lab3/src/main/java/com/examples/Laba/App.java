31375ab9d4ee8038cdf9503ce81b2d8104885fdc
Egorychev Artem <temych_2000@mail.ru>
2019-10-26 01:42:41 +0300

    Scripted auto-commit on change (2019-10-26 01:42:41) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/App.java
+++ lab3/src/main/java/com/examples/Laba/App.java
@@ -91,5 +91,6 @@ public class App {
 
                     return "from: " + AiroportDestID.get(key._1()) + "to: " + AiroportDestID.get(key._2());
                 });
+        result.saveAsTextFile("");
     }
 }
