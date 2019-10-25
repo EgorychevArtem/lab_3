f7b007d2232607a7f246dcbe7f213d2f1ab2b4b9
Egorychev Artem <temych_2000@mail.ru>
2019-10-25 20:01:20 +0300

    Scripted auto-commit on change (2019-10-25 20:01:20) by gitwatch.sh

--- lab3/src/main/java/com/examples/Laba/Serializabl.java
+++ lab3/src/main/java/com/examples/Laba/Serializabl.java
@@ -3,5 +3,5 @@ package com.examples.Laba;
 import java.io.Serializable;
 
 public class Serializabl implements Serializable {
-    public Serializabl(int AiroportID, )
+    public Serializabl(int AiroportID, int DestAiroportID, float delay, float)
 }
